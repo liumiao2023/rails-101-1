0aa79513a8d8f34096dec1ba8a7a47bb6e654396
liumiao2023 <liumiao2023@hotmail.com>
2016-07-19 00:47:51 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :groups do
     member do
@@ -8,5 +9,9 @@ Rails.application.routes.draw do
     resources :posts
   end
   root 'groups#index'
+
+  namespace :account do
+     resources :groups
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
